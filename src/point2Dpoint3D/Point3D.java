8249d1cd767f4bd9a08456ec7d61aae4101d663b
Mon0106 <dongnam.japan@gmail.com>
2020-07-10 11:06:27 +0700

    Point2D and Point3D

---
+++ src/point2Dpoint3D/Point3D.java
@@ -0,0 +1,40 @@
+package point2Dpoint3D;
+
+public class Point3D extends Point2D {
+    private float z = 0.0f;
+
+    public Point3D() {
+
+    }
+
+    public Point3D(float x, float y, float z){
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float[] arr = {x, y, z};
+        return arr;
+    }
+
+    public void setXYZ(float x, float y, float z){
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    @Override
+    public String toString(){
+        return "Point3D{ (x,y,z) = ("
+                + x + ", " + y + z
+                + ")}";
+    }
+}
---
+++ src/point2Dpoint3D/Test.java
@@ -0,0 +1,15 @@
+package point2Dpoint3D;
+
+public class Test {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2.5f,9.9f);
+        System.out.println(point2D);
+        Point3D point3D = new Point3D(2.2f,3.3f,4.4f);
+        System.out.println(point3D);
+//        point3D.setXYZ(5.5f,6.6f,7.7f);
+//        System.out.println(point3D);
+//        float[] arr = point2D.getXY();
+//        System.out.println(arr[1]);
+
+    }
+}
